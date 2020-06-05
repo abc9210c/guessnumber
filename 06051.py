6322d2aa7484542ea422c7d2245c60fc4a6253d8
George Hsieh <abc9210c@gmail.com>
2020-06-05 23:15:40 +0800

    fix

--- 06051.py
+++ 06051.py
@@ -1,7 +1,9 @@
 import random
 
 p = random.randint(1, 100)
+count = 0
 while True:
+	count += 1
 	num = input('猜猜數子: ')
 	num = int(num)
 	if num == p:
@@ -11,3 +13,4 @@ while True:
 		print('比答案大')
 	elif num < p:
 		print('比答案小')
+	print('這是猜的第', count, '次')
